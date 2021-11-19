5728d57ccc443769b24cab4c9e4402f6279d5131
atsushi.takano <atsushi.takano@every.tv>
2021-11-20 01:07:16 +0900

    Lesson1

--- app/src/main/java/me/aluceps/practicecompose/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicecompose/MainActivity.kt
@@ -1,10 +1,28 @@
 package me.aluceps.practicecompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Hello world!")
+        }
     }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
